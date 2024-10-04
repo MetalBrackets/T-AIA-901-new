a612e1c6f538ab7b4a2dc8e3117e82847bad017e
Jibe LELEVIER <devme@MSI.(none)>
2024-10-04 15:11:06 +0200

    feat(script): get_coordonates

--- README.md
+++ README.md
@@ -4,7 +4,3 @@
 
 📁 [Repository](https://github.com/MetalBrackets/T-AIA-901-new)  
 📁 [Mirror](https://github.com/EpitechMscProPromo2025/T-AIA-901-NAN_3)
-
-
-
-
---
+++ back/API/Utils/get_coordonates.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+
+# Access the CSV file
+file_path = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'data', 'liste-des-gares.csv')
+absolute_file_path = os.path.abspath(file_path)
+print(f"/// PATH : {absolute_file_path}")
+
+# Load station data
+def load_station_data():
+    df = pd.read_csv(absolute_file_path, delimiter=';')
+    # Rename 'latitude' and 'longitude'
+    df = df.rename(columns={'X_WGS84': 'longitude', 'Y_WGS84': 'latitude'})
+    # Drop rows with missing values in latitude and longitude
+    return df.dropna(subset=['latitude', 'longitude'])
+
+# Fetch les coordonnées d'une ville
+def get_coordinates(city_name, df):
+    """ Fetch les coordinates for a given city from the dataframe"""
+    if df is None:
+        return None, None
+    filtered_df = df[df['LIBELLE'].str.contains(city_name, case=False, na=False)]
+    if not filtered_df.empty:
+        lat = filtered_df.iloc[0]['latitude']
+        lon = filtered_df.iloc[0]['longitude']
+        return lat, lon
+    else:
+        print(f"No coordinates found for {city_name}")
+        return None, None
+
+# Example usage to test the script
+df = load_station_data()
+if df is not None:
+    start_city = "Nantes"
+    end_city = "Aixe-sur-Vienne"
+    start_coords = get_coordinates(start_city, df)
+    end_coords = get_coordinates(end_city, df)
+
+    print(f"Start coordinates for {start_city}: {start_coords}")
+    print(f"End coordinates for {end_city}: {end_coords}")
