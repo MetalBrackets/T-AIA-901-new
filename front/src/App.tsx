6d5d037802b96f30a28276e85d831e624e972295
Jibe LELEVIER <devme@MSI.(none)>
2024-11-14 13:58:39 +0100

    feat(back&front) small clean + new timeline composant

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,28 +1,23 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
-import MapComponent from './components/MapComponent';
-import RecordComponent from './components/RecordComponent';
+import type { Component } from "solid-js";
+import styles from "./App.module.css";
 import AudioComponent from "./components/AudioComponent";
-import DisplayMap from "./components/DisplayMap";
-import ItineraryInputComponent from "./components/ItineraryInputComponent";
 import TimelineComponent from "./components/TimelineComponent";
 
 const App: Component = () => {
   return (
-    <div>
-      {/*<RecordComponent />*/}
-        <div class="flex justify-center my-5">
-            <h1 class="text-4xl font-bold text-red-950">Train travel</h1>
+    <div class={styles.page}>
+      <div class={styles.header}>
+        <div class={styles.containerCenter}>
+          <h1 class="text-4xl font-bold">Réserver un train</h1>
         </div>
-        <AudioComponent/>
-        {/*<ItineraryInputComponent/>*/}
-        <TimelineComponent/>
-        <DisplayMap/>
-        {/*<MapComponent/>*/}
+        <div class={styles.containerCenter}>
+          <AudioComponent />
+        </div>
+      </div>
+      <div class={styles.containerCenter}>
+        <TimelineComponent />
+      </div>
     </div>
-     
   );
 };
 
