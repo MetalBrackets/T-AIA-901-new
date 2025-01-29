be6190c7ca7d376db125fad40c56aa81dc81afd0
Jibe LELEVIER <devme@MSI.(none)>
2025-01-29 19:06:29 +0100

    feat(ner): add ROC

--- back/test/utils.py
+++ back/test/utils.py
@@ -2,6 +2,7 @@
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 def encode_data(data, tokenizer, label_encoder):
     tokens = []
@@ -12,25 +13,6 @@ def encode_data(data, tokenizer, label_encoder):
         dep = row['Départ']
         arr = row['Arrivée']
 
-        dep_positions = []
-        arr_positions = []
-
-        start = 0  # Trouver toutes les occurrences de départ
-        while True:
-            idx = phrase.find(dep, start)
-            if idx == -1:
-                break
-            dep_positions.append((idx, idx + len(dep)))
-            start = idx + len(dep)
-
-        start = 0  # Trouver toutes les occurrences d'arrivée
-        while True:
-            idx = phrase.find(arr, start)
-            if idx == -1:
-                break
-            arr_positions.append((idx, idx + len(arr)))
-            start = idx + len(arr)
-
         tokenized_input = tokenizer.encode_plus(
             phrase,
             add_special_tokens=True,
@@ -39,34 +21,64 @@ def encode_data(data, tokenizer, label_encoder):
             return_tensors="tf"
         )
 
+        dep_positions = []
+        arr_positions = []
+
+        start = 0 # find all starting occurrences
+        while True:
+            idx = phrase.find(dep, start)
+            if idx == -1:
+                break
+            dep_positions.append((idx, idx + len(dep)))
+            start = idx + len(dep)
+
+        start = 0 # find all arrival occurrences
+        while True:
+            idx = phrase.find(arr, start)
+            if idx == -1:
+                break
+            arr_positions.append((idx, idx + len(arr)))
+            start = idx + len(arr)
+
         tokenized_text = tokenizer.convert_ids_to_tokens(tokenized_input.input_ids[0])
         offsets = tokenized_input['offset_mapping'].numpy()[0]
         label_list = ['O'] * len(tokenized_text)
 
-        for j, (offset_start, offset_end) in enumerate(offsets):  # Attribution des labels aux tokens
+        for j, (offset_start, offset_end) in enumerate(offsets):   # attribution of labels to tokens
             if offset_start == 0 and offset_end == 0:
-                continue  # Token de padding
-            token_label = 'O'
+                continue  # padding token
 
-            # Vérifier si le token chevauche une entité de départ
-            for entity_start, entity_end in dep_positions:
-                if (offset_start >= entity_start) and (offset_end <= entity_end):
+            token_label = 'O'
+            
+            for entity_start, entity_end in dep_positions: # check if the token overlaps an arrival entity
+                if offset_start >= entity_start and offset_end <= entity_end:
                     if offset_start == entity_start:
                         token_label = 'B-DEP'
                     else:
                         token_label = 'I-DEP'
                     break
 
-            # Vérifier si le token chevauche une entité d'arrivée
-            for entity_start, entity_end in arr_positions:
-                if (offset_start >= entity_start) and (offset_end <= entity_end):
-                    if offset_start == entity_start:
-                        token_label = 'B-ARR'
-                    else:
-                        token_label = 'I-ARR'
-                    break
+            if token_label == 'O': # check if the token overlaps an arrival entity
+                for entity_start, entity_end in arr_positions:
+                    if offset_start >= entity_start and offset_end <= entity_end:
+                        if offset_start == entity_start:
+                            token_label = 'B-ARR'
+                        else:
+                            token_label = 'I-ARR'
+                        break
 
             label_list[j] = token_label
+
+        # Ensure no 'I-xxx' is used without a preceding 'B-xxx' or 'I-xxx'
+        for k in range(1, len(label_list) - 1):  # exclude [CLS] and [SEP]
+            if label_list[k].startswith('I-'): 
+                entity_type = label_list[k][2:]
+                if k == 0: # if the first label in the list is 'I-xxx', change it to 'B-xxx'
+                    label_list[k] = 'B-' + entity_type
+                else: # else the previous label is not 'B-xxx' or 'I-xxx' of the same type, change 'I-xxx' to 'B-xxx'
+                    prev_label = label_list[k-1] # Get the previous label
+                    if not (prev_label == 'B-' + entity_type or prev_label == 'I-' + entity_type):
+                        label_list[k] = 'B-' + entity_type
 
         label_ids = label_encoder.transform(label_list)
         tokens.append(tokenized_input.input_ids.numpy()[0])
@@ -76,14 +88,53 @@ def encode_data(data, tokenizer, label_encoder):
     return np.array(tokens), np.array(labels)
 
 
-# def visualize_attention_heatmap(attention, tokens, model_name):
-#     plt.figure(figsize=(6, 5))
-#     sns.heatmap(attention, xticklabels=tokens, yticklabels=tokens, cmap='viridis')
-#     plt.title(f'Attention weights for {model_name}')
-#     plt.xlabel('Input Tokens')
-#     plt.ylabel('Output Tokens')
+def visualize_attention(text, model, tokenizer, model_name):
+    print("\n/////////////////////////////////////////////////////////////////////////////////////////////////")
+    print(f"\n---> Sentence: '{text}'")
+    print(f"\nIci on voit sur quels tokens d'entrées {model_name} porte son attention pour chaque token de sortie. Pour produire son embedding.")
+    # generate the outputs with the attentions
+    inputs = tokenizer.encode_plus(text, return_tensors='tf')
+    outputs = model(**inputs, output_attentions=True)
+    attentions = outputs.attentions  # (num_layers, batch, num_heads, seq_len, seq_len)
+    tokens = tokenizer.convert_ids_to_tokens(inputs['input_ids'][0].numpy())
 
-#     plt.show()
+    total_layers = len(attentions)
+    # select the last 5 layers
+    last_5_attentions = attentions[-5:]
+    num_layers = len(last_5_attentions)
+
+    # 1 heatmap par couche
+    fig, axes = plt.subplots(1, num_layers, figsize=(4 * num_layers, 4))
+    if num_layers == 1:
+        axes = [axes]
+
+    for layer_idx, layer_attention in enumerate(last_5_attentions):
+        layer_attention = tf.squeeze(layer_attention, axis=0)  # (num_heads, seq_len, seq_len)
+        layer_attention_mean = tf.reduce_mean(layer_attention, axis=0).numpy()
+
+        ax = axes[layer_idx]
+        sns.heatmap(
+            layer_attention_mean, 
+            xticklabels=tokens, 
+            yticklabels=tokens, 
+            cmap='viridis', 
+            ax=ax
+        )
+        # The layer index here is relative to the last 5 layers
+        absolute_layer_index = (len(attentions) - 5 + layer_idx + 1)
+        ax.set_title(f'Layer {absolute_layer_index}', fontsize=12)
+        ax.set_xlabel('Input Tokens', fontsize=10)
+        ax.set_ylabel('Output Tokens', fontsize=10)
+        # Label rotation
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+        ax.set_yticklabels(ax.get_yticklabels(), rotation=0)
+
+    fig.suptitle(f"Last 5 layers attention weights for {model_name}", fontsize=16)
+    plt.tight_layout()
+    plt.show()
+    print("Nombre total de layers:", total_layers)
+    print("Model summary:", model.summary())
+
 
 
 def print_predict_duration(total_start_time, total_end_time):
--- model/SANDRINE/utils.py
+++ model/SANDRINE/utils.py
@@ -3,6 +3,14 @@ import numpy as np
 from sklearn.metrics import confusion_matrix, classification_report
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.metrics import roc_curve, auc, roc_auc_score
+from sklearn.preprocessing import label_binarize
+from itertools import cycle
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import roc_curve, auc
+from itertools import cycle
+import tensorflow as tf
+
 
 def encode_data(data, tokenizer, label_encoder, max_length=36):
     tokens = []
@@ -20,7 +28,7 @@ def encode_data(data, tokenizer, label_encoder, max_length=36):
         dep_positions = []
         arr_positions = []
 
-        start = 0  # Trouver toutes les occurrences de départ
+        start = 0  # find all starting occurrences
         while True:
             idx = phrase.find(dep, start)
             if idx == -1:
@@ -28,7 +36,7 @@ def encode_data(data, tokenizer, label_encoder, max_length=36):
             dep_positions.append((idx, idx + len(dep)))
             start = idx + len(dep)
 
-        start = 0  # Trouver toutes les occurrences d'arrivée
+        start = 0  # find all arrival occurrences
         while True:
             idx = phrase.find(arr, start)
             if idx == -1:
@@ -53,22 +61,20 @@ def encode_data(data, tokenizer, label_encoder, max_length=36):
         if i < 4:
             print("- Encoded tokens: ", tokenized_text)
 
-        for j, (offset_start, offset_end) in enumerate(offsets):  # Attribution des labels aux tokens
+        for j, (offset_start, offset_end) in enumerate(offsets):  # attribution of labels to tokens
             if offset_start == 0 and offset_end == 0:
-                continue  # Token de padding
+                continue  # padding token
             token_label = 'O'
 
-            # Vérifier si le token chevauche une entité de départ
-            for entity_start, entity_end in dep_positions:
+            for entity_start, entity_end in dep_positions: # check if the token overlaps a starting entity
                 if (offset_start >= entity_start) and (offset_end <= entity_end):
                     if offset_start == entity_start:
                         token_label = 'B-DEP'
                     else:
                         token_label = 'I-DEP'
                     break
-
-            # Vérifier si le token chevauche une entité d'arrivée
-            for entity_start, entity_end in arr_positions:
+            
+            for entity_start, entity_end in arr_positions: # check if the token overlaps an arrival entity
                 if (offset_start >= entity_start) and (offset_end <= entity_end):
                     if offset_start == entity_start:
                         token_label = 'B-ARR'
@@ -77,6 +83,17 @@ def encode_data(data, tokenizer, label_encoder, max_length=36):
                     break
 
             label_list[j] = token_label
+
+        # Ensure no 'I-xxx' is used without a preceding 'B-xxx' or 'I-xxx'
+        for k in range(1, len(label_list) - 1):  # exclude [CLS] and [SEP]
+            if label_list[k].startswith('I-'): 
+                entity_type = label_list[k][2:]
+                if k == 0: # if the first label in the list is 'I-xxx', change it to 'B-xxx'
+                    label_list[k] = 'B-' + entity_type
+                else: # else the previous label is not 'B-xxx' or 'I-xxx' of the same type, change 'I-xxx' to 'B-xxx'
+                    prev_label = label_list[k-1] # Get the previous label
+                    if not (prev_label == 'B-' + entity_type or prev_label == 'I-' + entity_type):
+                        label_list[k] = 'B-' + entity_type
 
         if i < 4:
             print("- Labels after encoding the entities: ", label_list)
@@ -92,10 +109,59 @@ def encode_data(data, tokenizer, label_encoder, max_length=36):
     return np.array(tokens), np.array(labels)
 
 
+def get_ROC_curve(true_labels, logits, unique_labels):
+    n_labels = len(unique_labels)
+    y_true = true_labels.flatten()
+    
+    # Binarize real classes for ROC calculation (One-vs-Rest)
+    y_true_binarized = label_binarize(y_true, classes=range(n_labels)) #  0, 1, 2, ..., n_labels-1
+
+    # Moving from logits to probabilities via a softmax
+    probabilities = tf.nn.softmax(logits, axis=-1).numpy()  # shape (N, seq_len, n_labels)
+    probabilities = probabilities.reshape(-1, n_labels)     # shape (N*seq_len, n_labels)
+
+    # Calculate fpr, tpr and AUC for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+
+    for i in range(n_labels):
+        fpr[i], tpr[i], _ = roc_curve(y_true_binarized[:, i], probabilities[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Calcul of the micro-average of all classes
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_true_binarized.ravel(), probabilities.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    plt.figure(figsize=(8, 6))
+    plt.plot(
+        fpr["micro"], tpr["micro"],
+        label="Micro-average ROC (AUC = {0:0.2f})".format(roc_auc["micro"]),
+        color="deeppink", linestyle=":", linewidth=4
+    )
+    colors = cycle(["aqua", "darkorange", "cornflowerblue", "green", "red", "purple"])
+    for (i, color) in zip(range(n_labels), colors):
+        plt.plot(
+            fpr[i], tpr[i], color=color, lw=2,
+            label="Classe '{0}' (AUC = {1:0.2f})".format(unique_labels[i], roc_auc[i])
+        )
+
+    print("\nROC multiclasses curve:")
+    plt.plot([0, 1], [0, 1], "k--", lw=2)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel("Taux de Faux Positifs (FPR)")
+    plt.ylabel("Taux de Vrais Positifs (TPR)")
+    plt.title("ROC multiclasses")
+    plt.legend(loc="lower right")
+    plt.show()
+
+
 def get_metrics(true_labels, predicted_labels, unique_labels, history=None):
     cm = confusion_matrix(true_labels, predicted_labels)
     cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis] # normalisation
 
+    print("\nConfusion Matrix %:")
     # Heatmap
     plt.figure(figsize=(10, 7))
     custom_palette = ["#DCDED6", "#CED0C3", "#B4BAB1", "#859393", "#5D726F", "#485665"]
